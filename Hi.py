215ce11391508bea04a6cb6bc51fe6bcaa5d0c3c
MujtabaG45 <mujtabag1093@gmail.com>
2025-09-20 13:48:01 +0000

    Oooo a Skbiid

--- Hi.py
+++ Hi.py
@@ -6,7 +6,7 @@ a = input() # Takes in the User Input and Stores it in a Variable
 def Message():
     print("Hi!, It's Mujtaba!, And i hijacked a Github Action to run Code server! xD") # The Message the Password is Protecting,
     
-if a == "Password": # Sets the Password and if correct prints Message(): But if not then says "Acess Denied!"
+if a == "Password": # Sets the Password and if correct prints Message(): But if not then says "Access Denied"
     Message()
 else:
     print("Access Denied!, Keep on guessing! (Or just look in the python file, I guess that works.)") 
