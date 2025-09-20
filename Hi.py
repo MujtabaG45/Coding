4667870a928ca4cd208a6f69b537629b9b1653a0
MujtabaG45 <mujtabag1093@gmail.com>
2025-09-20 12:50:58 +0000

    Added Comments to the Code

--- Hi.py
+++ Hi.py
@@ -1,10 +1,12 @@
-print("Please type in the password:")
+print("Please type in the password:") # Asks the user to put in the password
+ 
 
-
-a = input()
+a = input() # Takes in the User Input and Stores it in a Variable
 
 def Message():
-    print("Hi!, It's Mujtaba!")
+    print("Hi!, It's Mujtaba!, And i hijacked a Github Action to run Code server! xD") # The Message the Password is Protecting,
     
-if a == "Password":
+if a == "Password": # Sets the Password and if correct prints Message(): But if not then says "Acess Denied!"
     Message()
+else:
+    print("Access Denied!, Keep on guessing! (Or just look in the python file, I guess that works.)") 
